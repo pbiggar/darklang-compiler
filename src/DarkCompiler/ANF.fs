119dd5da56b922bee5c87f143939e36c9fef892e
Paul Biggar <paul.biggar@gmail.com>
2025-12-22 11:07:55 +0100

    Define ANF IR data structures

---
+++ src/DarkCompiler/ANF.fs
@@ -0,0 +1,39 @@
+module ANF
+
+/// Unique identifier for temporary variables
+type TempId = TempId of int
+
+/// Atomic expressions (cannot be decomposed further)
+type Atom =
+    | IntLiteral of int64
+    | Var of TempId
+
+/// Binary operations on atoms
+type BinOp =
+    | Add
+    | Sub
+    | Mul
+    | Div
+
+/// Complex expressions (produce values)
+type CExpr =
+    | Atom of Atom
+    | Prim of BinOp * Atom * Atom
+
+/// ANF expressions with explicit sequencing
+type AExpr =
+    | Let of TempId * CExpr * AExpr
+    | Return of Atom
+
+/// ANF program
+type Program = Program of AExpr
+
+/// Fresh variable generator (functional style)
+type VarGen = VarGen of int
+
+/// Generate a fresh temporary variable
+let freshVar (VarGen n) : TempId * VarGen =
+    (TempId n, VarGen (n + 1))
+
+/// Initial variable generator
+let initialVarGen = VarGen 0
