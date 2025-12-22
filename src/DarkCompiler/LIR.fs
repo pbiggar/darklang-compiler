c9c36284f6455d748fc2615b91e3c13f8606d9d7
Paul Biggar <paul.biggar@gmail.com>
2025-12-22 11:20:31 +0100

    Implement register allocation

--- src/DarkCompiler/LIR.fs
+++ src/DarkCompiler/LIR.fs
@@ -137,10 +137,92 @@ let toLIR (program: MIR.Program) : Program =
 
     Program [func]
 
+/// Collect all virtual registers used in instructions
+let collectVirtualRegs (instrs: Instr list) : Set<int> =
+    let rec collect (instr: Instr) (acc: Set<int>) : Set<int> =
+        match instr with
+        | Mov (Virtual id, _) -> Set.add id acc
+        | Mov (_, Reg (Virtual id)) -> Set.add id acc
+        | Add (Virtual id, Virtual leftId, Reg (Virtual rightId)) ->
+            acc |> Set.add id |> Set.add leftId |> Set.add rightId
+        | Add (Virtual id, Virtual leftId, _) -> acc |> Set.add id |> Set.add leftId
+        | Add (Virtual id, _, Reg (Virtual rightId)) -> acc |> Set.add id |> Set.add rightId
+        | Add (Virtual id, _, _) -> Set.add id acc
+        | Sub (Virtual id, Virtual leftId, Reg (Virtual rightId)) ->
+            acc |> Set.add id |> Set.add leftId |> Set.add rightId
+        | Sub (Virtual id, Virtual leftId, _) -> acc |> Set.add id |> Set.add leftId
+        | Sub (Virtual id, _, Reg (Virtual rightId)) -> acc |> Set.add id |> Set.add rightId
+        | Sub (Virtual id, _, _) -> Set.add id acc
+        | Mul (Virtual id, Virtual left, Virtual right) ->
+            acc |> Set.add id |> Set.add left |> Set.add right
+        | Mul (Virtual id, Virtual left, _) -> acc |> Set.add id |> Set.add left
+        | Mul (Virtual id, _, Virtual right) -> acc |> Set.add id |> Set.add right
+        | Mul (Virtual id, _, _) -> Set.add id acc
+        | Sdiv (Virtual id, Virtual left, Virtual right) ->
+            acc |> Set.add id |> Set.add left |> Set.add right
+        | Sdiv (Virtual id, Virtual left, _) -> acc |> Set.add id |> Set.add left
+        | Sdiv (Virtual id, _, Virtual right) -> acc |> Set.add id |> Set.add right
+        | Sdiv (Virtual id, _, _) -> Set.add id acc
+        | _ -> acc
+
+    instrs |> List.fold (fun acc instr -> collect instr acc) Set.empty
+
+/// Available physical registers for allocation (avoiding X0 which is for return)
+let availableRegs = [X1; X2; X3; X4; X5; X6; X7; X8; X9; X10; X11; X12; X13; X14; X15]
+
+/// Create allocation map for virtual registers
+let createAllocation (virtualRegs: Set<int>) : Map<int, PhysReg> =
+    let sorted = Set.toList virtualRegs |> List.sort
+    // Pair up virtual registers with physical registers
+    // If we have more virtual than physical registers, truncate physical
+    // If we have fewer virtual, truncate availableRegs
+    let regsToUse = List.truncate sorted.Length availableRegs
+    List.zip sorted regsToUse
+    |> Map.ofList
+
+/// Apply allocation to a register
+let applyToReg (allocation: Map<int, PhysReg>) (reg: Reg) : Reg =
+    match reg with
+    | Physical p -> Physical p
+    | Virtual id ->
+        match Map.tryFind id allocation with
+        | Some physReg -> Physical physReg
+        | None -> Physical X1  // Fallback (shouldn't happen)
+
+/// Apply allocation to an operand
+let applyToOperand (allocation: Map<int, PhysReg>) (operand: Operand) : Operand =
+    match operand with
+    | Imm n -> Imm n
+    | Reg reg -> Reg (applyToReg allocation reg)
+    | StackSlot s -> StackSlot s
+
+/// Apply allocation to an instruction
+let applyAllocation (allocation: Map<int, PhysReg>) (instr: Instr) : Instr =
+    match instr with
+    | Mov (dest, src) ->
+        Mov (applyToReg allocation dest, applyToOperand allocation src)
+    | Add (dest, left, right) ->
+        Add (applyToReg allocation dest, applyToReg allocation left, applyToOperand allocation right)
+    | Sub (dest, left, right) ->
+        Sub (applyToReg allocation dest, applyToReg allocation left, applyToOperand allocation right)
+    | Mul (dest, left, right) ->
+        Mul (applyToReg allocation dest, applyToReg allocation left, applyToReg allocation right)
+    | Sdiv (dest, left, right) ->
+        Sdiv (applyToReg allocation dest, applyToReg allocation left, applyToReg allocation right)
+    | Ret -> Ret
+
 /// Register allocation
 let allocateRegisters (func: Function) : AllocResult =
-    // To be implemented
+    // Collect virtual registers
+    let virtualRegs = collectVirtualRegs func.Body
+
+    // Create allocation map
+    let allocation = createAllocation virtualRegs
+
+    // Apply allocation to all instructions
+    let allocatedInstrs = func.Body |> List.map (applyAllocation allocation)
+
     {
-        Instrs = func.Body
-        StackSize = 0
+        Instrs = allocatedInstrs
+        StackSize = 0  // No spilling yet
     }
