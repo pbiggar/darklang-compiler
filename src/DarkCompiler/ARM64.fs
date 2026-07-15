46ae6ed3cbd33618b866ff39fc4d795eccd4ce11
Paul Biggar <paul.biggar@gmail.com>
2026-07-15 05:31:07 +0000

    Clarify ARM64 reserved register comments

--- src/DarkCompiler/ARM64.fs
+++ src/DarkCompiler/ARM64.fs
@@ -27,8 +27,9 @@ module ARM64
 type Reg =
     | X0 | X1 | X2 | X3 | X4 | X5 | X6 | X7 | X8 | X9
     | X10 | X11 | X12 | X13 | X14 | X15 | X16 | X17  // X16/X17 are IP0/IP1 scratch registers
-    | X19 | X20 | X21 | X22 | X23 | X24 | X25 | X26 | X27  // Callee-saved
-    | X28  // Reserved for heap pointer
+    | X19 | X20 | X21 | X22 | X23 | X24 | X25 | X26  // Callee-saved
+    | X27  // Reserved for free list base pointer
+    | X28  // Reserved for heap bump pointer
     | X29 | X30 | SP
 
 /// ARM64 floating-point registers (D0-D31 for double precision)
