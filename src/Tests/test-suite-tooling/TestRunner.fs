e0c5b8c02275dc12e8a7ff4ab77782b229af797d
Paul Biggar <paul.biggar@gmail.com>
2026-07-15 11:52:18 +0000

    Remove CLI flag plumbing unit test

--- src/Tests/test-suite-tooling/TestRunner.fs
+++ src/Tests/test-suite-tooling/TestRunner.fs
@@ -277,7 +277,6 @@ let private runTestsWithProgressReporter (completedTestReporter: (int -> unit) o
 
     let unitStdlibSuites = [ "Stdlib Compile Tests"; "Preamble Build Tests" ]
     let buildUnitTests (_stdlib: CompilerLibrary.StdlibResult) : UnitTestSuite array = [|
-        { Name = "CLI Flags Tests"; Tests = CliFlagTests.tests }
         { Name = "IR Symbol Tests"; Tests = IRSymbolTests.tests }
         { Name = "IR Printer Tests"; Tests = IRPrinterTests.tests }
         { Name = "ANF to MIR Tests"; Tests = ANFToMIRTests.tests }
