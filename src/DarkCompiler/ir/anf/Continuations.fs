e83a1d6448fb9d38cac161df909bc5dfebde6ab9
Paul Biggar <paul.biggar@gmail.com>
2026-09-19 01:43:31 +0200

    Lower matches in ANF atom positions

--- src/DarkCompiler/ir/anf/Continuations.fs
+++ src/DarkCompiler/ir/anf/Continuations.fs
@@ -2,6 +2,13 @@
 
 module ANFContinuations
 
+let isSupportedJoinArgumentType = function
+    | AST.TInt8 | AST.TInt16 | AST.TInt32 | AST.TInt64
+    | AST.TUInt8 | AST.TUInt16 | AST.TUInt32 | AST.TUInt64
+    | AST.TBool | AST.TDateTime | AST.TUnit
+    | AST.TRawPtr -> true
+    | _ -> false
+
 let rec bindReturns (expr: ANF.AExpr) (k: ANF.Atom -> ANF.AExpr) : ANF.AExpr =
     match expr with
     | ANF.Jump _ -> expr
